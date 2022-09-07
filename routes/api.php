b09f99d66553b398249b7dcb5a5a090ce4823e7b
locnhph16486 <locnhph16486@fpt.edu.vn>
2022-09-07 22:40:59 +0700

    'TEP-17'

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\MajorController;
+use App\Models\Major;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +23,16 @@ use Illuminate\Support\Facades\Route;
 //     Route::put('update/{user}', 'UserController@update');
 //     Route::delete('delete/{id}', 'UserController@delete');
 // });
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::name('major')->prefix('major')->group(function () {
+        Route::get('get-all', [MajorController::class, 'index'])->name('index');
+        Route::get('show/{id}', [MajorController::class, 'show'])->name('show');
+        Route::middleware('admin')->group((function () {
+            Route::post('store', [MajorController::class, 'store'])->name('store');
+            Route::put('update/{id}', [MajorController::class, 'update'])->name('update');
+            Route::delete('destroy/{id}', [MajorController::class, 'destroy'])->name('destroy');
+        }));
+    });
+    
+});

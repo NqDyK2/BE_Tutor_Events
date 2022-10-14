cfba0bc31caa41bc59b8469cfcbaac6d1470ad3d
ldt <the.ld@zinza.com.vn>
2022-10-14 15:42:53 +0700

    TEP-175 restore lessonServices

--- app/Services/AuthServices.php
+++ app/Services/AuthServices.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Services;
 
@@ -20,41 +20,50 @@ class AuthServices
                 'name' => $googleUser->name,
                 'email' => $googleUser->email,
             ]);
-        }
-        elseif ($user->status == USER_STATUS_DEACTIVATE) {
+        } elseif ($user->status == USER_STATUS_DEACTIVATE) {
             return response([
                 'message' => 'This account has been blocked',
             ], 403);
         }
-        
+
         return $user->createToken('API TOKEN')->plainTextToken;
     }
 
     public function getAuthDetail()
     {
-        $auth = Auth::user();
+        $auth = Auth::user()->only([
+            'id',
+            'name',
+            'code',
+            'email',
+            'avatar',
+            'role_id'
+        ]);
+        if ($auth['role_id'] == 1) return $auth;
 
         $isTeacher = Lesson::join('classrooms', 'classrooms.id', 'lessons.classroom_id')
-        ->join('semesters', 'semesters.id', 'classrooms.semester_id')
-        ->where('semesters.start_time', '<=', now())->where('semesters.end_time', '>=', now())
-        ->where('lessons.teacher_email', $auth->email)
-        ->exists();
+            ->join('semesters', 'semesters.id', 'classrooms.semester_id')
+            ->where('semesters.start_time', '<=', now())->where('semesters.end_time', '>=', now())
+            ->where('lessons.teacher_email', $auth['email'])
+            ->exists();
+
 
         if ($isTeacher) {
-            $auth->role_id = 2;
+            $auth['role_id'] = 2;
             return $auth;
         }
-        
+
         $isTutor = Lesson::join('classrooms', 'classrooms.id', 'lessons.classroom_id')
-        ->join('semesters', 'semesters.id', 'classrooms.semester_id')
-        ->where('semesters.start_time', '<=', now())->where('semesters.end_time', '>=', now())
-        ->where('lessons.tutor_email', $auth->email)
-        ->exists();
+            ->join('semesters', 'semesters.id', 'classrooms.semester_id')
+            ->where('semesters.start_time', '<=', now())->where('semesters.end_time', '>=', now())
+            ->where('lessons.tutor_email', $auth['email'])
+            ->exists();
 
         if ($isTutor) {
-            $auth->role_id = 4;
+            $auth['role_id'] = 4;
             return $auth;
         }
 
+        return $auth;
     }
 }

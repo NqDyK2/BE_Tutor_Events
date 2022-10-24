235962d6723a1234433e41842573820e99f1bc10
ldt <the.ld@zinza.com.vn>
2022-10-24 11:46:55 +0700

    TEP-211 update major feature

--- app/Http/Controllers/Api/MajorController.php
+++ app/Http/Controllers/Api/MajorController.php
@@ -5,10 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Major\CreateMajorRequest;
 use App\Http\Requests\Major\UpdateMajorRequests;
-use App\Models\Major;
 use Illuminate\Http\Request;
 use App\Services\MajorServices;
-use GuzzleHttp\Promise\Create;
 
 class MajorController extends Controller
 {
@@ -25,62 +23,33 @@ class MajorController extends Controller
 
         return response([
             'data' => $majors
-        ],200);
+        ], 200);
     }
 
     public function store(CreateMajorRequest $request)
     {
-        $majors = $this->majorServices->create($request->input());
+        $this->majorServices->create($request->input());
 
-        if ($majors){
-            return response([
-                'massage' => 'Tạo mới chuyên ngành thành công',
-            ],201);
-        }else{
-            return response([
-                'massage' => 'Tạo mới chuyên ngành thất bại'
-            ],400);
-        }
-    }
-
-    public function show(Request $request)
-    {
-        $major = $request->get('major');
         return response([
-            'data' => $major
-        ],200);
+            'massage' => 'Tạo mới chuyên ngành thành công',
+        ], 201);
     }
 
     public function update(UpdateMajorRequests $request)
     {
         $major = $request->get('major');
 
-        $majors = $this->majorServices->update($request->input() , $major);
+        $this->majorServices->update($request->input(), $major);
 
-        if ($majors){
-            return response([
-                'massage' => 'Cập nhật chuyên ngành thành công',
-            ],201);
-        }else{
-            return response([
-                'massage' => 'Cập nhật chuyên ngành thất bại'
-            ],400);
-        }
+        return response([
+            'massage' => 'Cập nhật chuyên ngành thành công',
+        ], 201);
     }
 
     public function destroy(Request $request)
     {
-        $major = $request->get('major');
-        $majorDelete = $this->majorServices->destroy($major);
+        $response = $this->majorServices->destroy($request->major_id);
 
-        if($majorDelete){
-            return response([
-                'message' => 'Delete Major successfully',
-            ],200);
-        } else {
-            return response([
-                'massage' => 'Delete Major false'
-            ],400);
-        }
+        return $response;
     }
 }

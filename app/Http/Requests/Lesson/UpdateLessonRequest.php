650625fda14227b4cbfc2fcddef2d1d4d0458d0c
Dam Minh Hieu <daminhieu1703@gmail.com>
2023-01-09 22:08:55 +0700

    TEP-346 merge tep nay di cu the oi

--- app/Http/Requests/Lesson/UpdateLessonRequest.php
+++ app/Http/Requests/Lesson/UpdateLessonRequest.php
@@ -60,7 +60,7 @@ class UpdateLessonRequest extends FormRequest
             'type.required' => 'Type không được để trống',
             'type.boolean' => 'Type không đúng định dạng',
 
-            'teacher_email.email' => 'Email giảng viên không được để trống',
+            'teacher_email.email' => 'Email giảng viên không đúng định dạng',
             'tutor_email.email' => 'Email tutor không đúng định dạng',
 
             'content.max' => 'Nội dung không được quá 2000 ký tự',
